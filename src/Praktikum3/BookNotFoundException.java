d7f92d1eaeeffdcb6a7f5b27723e014cde4dac81
Fata <indrafataazhari@gmail.com>
2025-10-08 01:41:12 +0700

    Tambahkan ulang folder Praktikum3 sebagai folder biasa dengan file Java

---
+++ src/Praktikum3/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package Praktikum3;
+
+public class BookNotFoundException extends Exception {
+    public BookNotFoundException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/Praktikum3/User.java
@@ -0,0 +1,16 @@
+package Praktikum3;
+
+public class User {
+    private String id;
+    private String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name;
+    }
+}
